74270f99da5938f751b97d7be1c4023173f8257d
Briana122 <lee.briana@gmail.com>
2021-08-11 17:06:40 -0400

    added page that has sign in and create account options

--- choresClient/app/src/main/java/com/example/choresclient/MainActivity.java
+++ choresClient/app/src/main/java/com/example/choresclient/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.choresclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button signInBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInBtn = findViewById(R.id.btnSignIn);
+        signInBtn.setOnClickListener(this);
+
+        signUpBtn = findViewById(R.id.btnSignUp);
+        signUpBtn.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.btnSignIn) {
+            setContentView(R.layout.activity_signin);
+        }
+        else if (view.getId() == R.id.btnSignUp) {
+            setContentView(R.layout.activity_signup);
+        }
+
+    }
+
 }
