996f9b8b3d503b0051f761c5c1db38b9d6191b47
Melody Cheng (UWaterloo account) <mgkcheng@uwaterloo.ca>
2021-08-20 10:42:57 -0700

    Add due date field

--- choresServer/choresServer/task/models.py
+++ choresServer/choresServer/task/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils import timezone
+import datetime
+
 class Task(models.Model):
     user = models.ForeignKey(
         User,
@@ -10,3 +13,4 @@ class Task(models.Model):
     )
     room = models.CharField(max_length=20)
     task = models.CharField(max_length=60)
+    due_date = models.DateTimeField(default=timezone.now)
--- choresServer/choresServer/task/views.py
+++ choresServer/choresServer/task/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 
+from datetime import datetime, timedelta
+
 from .models import Task
 
 @csrf_exempt
@@ -13,13 +15,23 @@ def all_tasks(request, user_id):
     User = get_user_model()
     try:
         user = User.objects.get(id=user_id)
+        due_date = request.GET.get('due_date')
+        due_date_object = datetime.strptime(due_date, '%Y-%m-%d')
+        due_date_object += timedelta(days=1)
 
-        tasksObject = Task.objects.filter(user=user)
+        if due_date:
+            tasksObject = Task.objects.filter(
+                user=user,
+                due_date__lte=due_date_object
+            )
+        else:
+            tasksObject = Task.objects.filter(user=user)
         tasksList = []
         for task in tasksObject:
             tasksList.append({
                 'room': getattr(task, 'room'),
-                'task': getattr(task, 'task')
+                'task': getattr(task, 'task'),
+                'due_date': getattr(task, 'due_date')
             })
 
         return Response(
