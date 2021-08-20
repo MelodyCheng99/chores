8f6139f14480c562a03e23eae87f873088ba76f2
Melody Cheng (UWaterloo account) <mgkcheng@uwaterloo.ca>
2021-08-19 23:05:44 -0700

    Sign in & show welcome message

--- choresClient/app/src/main/java/com/example/choresclient/CustomAsyncTask.java
+++ choresClient/app/src/main/java/com/example/choresclient/CustomAsyncTask.java
@@ -6,12 +6,20 @@ import android.util.Log;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 
 // The following code is taken from
 // https://stackoverflow.com/questions/19229204/android-waiting-for-response-from-server
@@ -33,6 +41,35 @@ public class CustomAsyncTask extends AsyncTask<String, String, String> {
         try {
             url = new URL(strings[0]);
             urlConnection = (HttpURLConnection) url.openConnection();
+
+            // For POST requests
+            // TODO: Need to add support for adding arguments to GET requests
+            if (strings.length > 1) {
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+
+                String requestMethod = strings[1];
+                urlConnection.setRequestMethod(requestMethod);
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                List<AbstractMap.SimpleEntry<String, String>> params = new ArrayList<>();
+                int stringsPos = 2;
+                while (stringsPos < strings.length) {
+                    params.add(new AbstractMap.SimpleEntry<String, String>(strings[stringsPos], strings[stringsPos + 1]));
+                    stringsPos += 2;
+                }
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getQuery(params));
+                writer.flush();
+                writer.close();
+                os.close();
+            }
+
+            urlConnection.connect();
 
             int responseCode = urlConnection.getResponseCode();
 
@@ -84,5 +121,24 @@ public class CustomAsyncTask extends AsyncTask<String, String, String> {
             }
         }
         return response.toString();
+    }
+
+    private String getQuery(List<AbstractMap.SimpleEntry<String, String>> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (AbstractMap.SimpleEntry<String, String> pair : params)
+        {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(pair.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 }
